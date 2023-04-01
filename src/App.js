b64832756e4a8210a9fbd88cf549c062856771fc
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-01 17:07:22 -0400

    imported, initilized, and delcared useState for the Square component.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@
 // File's Purpose - Creates the Square and Board (gameboard) components. The Board component is composed of 3x3 sets of Squares component for 
 // the project. The board component will render the Square component. Written in JSX.
 // Date - 2023/31/03
+import { useState } from 'react';
 
 function Square({ value }) {
+  const [value, setValue] = useState(null);
+
   function handleClick() {
     console.log('clicked!');
   }
