e8e3aafd88f0c9723b50dd29b166c6673a709ce3
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-02 03:45:49 -0400

    Commented out experimental code. Continue on with tutorial.

--- src/App.js
+++ src/App.js
@@ -45,18 +45,18 @@ export default function Board() {
 
   // Figuring out how to set an array of n size.
   // https://stackoverflow.com/questions/3746725/how-to-create-an-array-containing-1-n
-  let boardsize = 3;
-  let objects = Array.from({ length: boardsize }, (_, i) => i + 1);
-  let set = Array.from(Array(boardsize * boardsize).keys());
-  console.log(set);
-  console.log(objects);
+  // let boardsize = 3;
+  // let objects = Array.from({ length: boardsize }, (_, i) => i + 1);
+  // let set = Array.from(Array(boardsize * boardsize).keys());
+  // console.log(set);
+  // console.log(objects);
 
 
   return (
     <>
       {/*  Figuring out how to generate loops in JSX
       https://stackoverflow.com/questions/22876978/loop-inside-react-jsx */}
-      <div className="board-row">
+      {/* <div className="board-row">
         {
             objects.map(function (row, r) {
               return (
@@ -74,7 +74,7 @@ export default function Board() {
               )
             })
           }
-      </div>  
+      </div>   */}
       
       <div>      
         <div className="status">{status}</div>
