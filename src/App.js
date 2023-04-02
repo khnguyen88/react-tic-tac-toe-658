0327278135c2406d0a58921ae75f701450c5308d
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-02 15:37:48 -0400

    Added and exported a new component called Game and added the Board component into the Game template.

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function Square({ value, onSquareClick }) {
 
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -79,6 +79,21 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board/>
+      </div>
+      <div className="game-info">
+        <ol>
+          {/* TODO */}
+        </ol>
+      </div>
+    </div>
+  )
 }
 
 function calculateWinner(squares) {
