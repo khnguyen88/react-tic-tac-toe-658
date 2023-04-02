6f0e78f3bc4ae2618e8ba1337060773cb0b06c10
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-02 19:53:18 -0400

    Modified the Game component to render the board current selected move or set of squares from that selected move, and not the latest move or set of squares.

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
 
@@ -106,7 +106,7 @@ export default function Game() {
     setXIsNext(nextMove % 2 === 0);
   }
 
-  const moves = history.map((squares, move) => { 
+  const moves = history.map((currentSquares, move) => { 
     let description;
     if (move > 0) {
       description = 'Go to move #' + move;
@@ -115,7 +115,7 @@ export default function Game() {
       description = 'Go to start game';
     }
     return (
-      // Placeholder key prop required for each child in an array or iterator
+      // Key prop required for each child in an array or iterator
       <li key={move}>
         <button onClick={()=> jumpTo(move)}>
           {description}
