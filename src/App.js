00301c89ff5c4742404d8c6df8ec71cdc2d0d697
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-02 03:40:45 -0400

    EXPERIMENTAL: Save code to try and generate grid, very messy. Try a different approach. for 2d grid. NOT WORKING!

--- src/App.js
+++ src/App.js
@@ -46,16 +46,32 @@ export default function Board() {
   // Figuring out how to set an array of n size.
   // https://stackoverflow.com/questions/3746725/how-to-create-an-array-containing-1-n
   let boardsize = 3;
-  let objects = Array.from(Array(boardsize).keys());
+  let objects = Array.from({ length: boardsize }, (_, i) => i + 1);
+  let set = Array.from(Array(boardsize * boardsize).keys());
+  console.log(set);
+  console.log(objects);
+
 
   return (
     <>
       {/*  Figuring out how to generate loops in JSX
       https://stackoverflow.com/questions/22876978/loop-inside-react-jsx */}
       <div className="board-row">
-          {
-            objects.map(function (object, i) {
-                return <Square value={squares[object]} key={i} onSquareClick={() => { handleClick(object) }} />;
+        {
+            objects.map(function (row, r) {
+              return (
+                <div className="board-row">
+                  {
+                    objects.map(function (col, c) {
+                      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+                      // how to remove first element of an array and retrieve it's value
+                      // figured retrieve value of the first element multiple times and pop it at the end
+                      // BAD CAN'T SET THE value for
+                      return <Square value={squares[set[0]]} key={set.shift()}  onSquareClick={(key) => { handleClick(key) }} />;
+                    })
+                  }
+                </div>  
+              )
             })
           }
       </div>  
