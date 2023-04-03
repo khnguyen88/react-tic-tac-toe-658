e0dc112310cfade89bbbbd970c4b1d70d4eb3a68
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-02 23:15:10 -0400

    Removed uncessary old and commented out code block.

--- src/App.js
+++ src/App.js
@@ -72,28 +72,6 @@ function Board({ xIsNext, squares, onPlay }) {
           })
         }
       </div>
-      
-      {/* COMMENT OUT FOR NOW TO PREVENT CODING ERROR */}
-      {/* <div>      
-        <div className="status">{status}</div>
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => { handleClick(0) }}/>
-          <Square value={squares[1]} onSquareClick={() => { handleClick(1) }}/>
-          <Square value={squares[2]} onSquareClick={() => { handleClick(2) }}/>
-        </div>
-
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => { handleClick(3) }}/>
-          <Square value={squares[4]} onSquareClick={() => { handleClick(4) }}/>
-          <Square value={squares[5]} onSquareClick={() => { handleClick(5) }}/>
-        </div>
-
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => { handleClick(6) }}/>
-          <Square value={squares[7]} onSquareClick={() => { handleClick(7) }}/>
-          <Square value={squares[8]} onSquareClick={() => { handleClick(8) }}/>
-        </div>
-      </div> */}
     </>
   );
 }
@@ -208,13 +186,5 @@ function calculateWinner(squares) {
     }
   }
 
-
-  // COMMENT OUT FOR NOW TO PREVENT CODING ERROR
-  // for (let i = 0; i < lines.length; i++){
-  //   const [a, b, c] = lines[i];
-  //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-  //     return squares[a];
-  //   }
-  // }
   return null;
 }
